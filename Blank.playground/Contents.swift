c189f9fb052d01c7faef64116890c1c892e8add4
Mike Schweitzer <mschweitzer@gmail.com>
2020-07-05 00:53:45 -0700

    Positional arguments! Apparently it's POSIX style =\

--- Blank.playground/Contents.swift
+++ Blank.playground/Contents.swift
@@ -1,3 +1,6 @@
 
 let counter = Counter(2)
 counter.display()
+
+let foo = Foo(pronoun: "my", number: 42)
+print(foo.description)
--- Blank.playground/Sources/Foo.swift
+++ Blank.playground/Sources/Foo.swift
@@ -1,15 +1,17 @@
 import Foundation
 
 public struct Foo: CustomStringConvertible {
-    let formatStr = "Answer to life is %d"
-    var someInt:Int
+    let formatStr = "Answer to %2$@ life is %1$d"
+    var pronoun:String
+    var number:Int
     
-    public init(_ number:Int)
+    public init(pronoun:String, number:Int)
     {
-        someInt = number
+        self.pronoun = pronoun
+        self.number = number
     }
     
     public var description:String {
-        return String(format:formatStr, someInt)
+        return String(format:formatStr, number, pronoun)
     }
 }
