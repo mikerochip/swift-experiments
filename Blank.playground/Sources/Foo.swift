6b372289edee7b89b8f348691991fcce372a3250
Mike Schweitzer <mschweitzer@gmail.com>
2020-07-04 17:18:10 -0700

    Committed all of my current playgrounds and projects

---
+++ Blank.playground/Sources/Foo.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+public struct Foo: CustomStringConvertible
+{
+    let formatStr = "Answer to life is %d"
+    var someInt:Int
+    
+    public init(_ number:Int)
+    {
+        someInt = number
+    }
+    
+    public var description:String {
+        return String(format:formatStr, someInt)
+    }
+}
